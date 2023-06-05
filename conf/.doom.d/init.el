aa495aa095f1c82e4958184625dffc9e02fc5ef7
Nathanaël Semhoun <nathanael@semhoun.net>
2023-06-05 16:57:01 +0200

    Add treemacs

--- conf/.doom.d/init.el
+++ conf/.doom.d/init.el
@@ -21,11 +21,11 @@
        (layout +azerty)    ; auie,ctsrnm is the superior home row
 
        :completion
-       company             ; the ultimate code completion backend
+       ;;company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       vertico             ; the search engine of the future
+       (vertico +icons)    ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -44,7 +44,7 @@
        ophints             ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       treemacs            ; a project drawer, like neotree but cooler
+       (treemacs +lsp)     ; a project drawer, like neotree but cooler
        unicode             ; extended unicode support for various languages
        vc-gutter ; vcs diff in the fringe
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
